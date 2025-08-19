bb083febefc014dc78e66a6e6f1548235f6037f2
Hissal <hissalofficial@gmail.com>
2025-08-19 15:25:26 +0300

    rename mod-01 to mod-02 & naming fixes

---
+++ mod-02/t-6.py
@@ -0,0 +1,16 @@
+﻿import random
+
+def random_number_chain(length: int, min: int, max: int) -> list[int]:
+    arr = []
+    for _ in range(length):
+        arr.append(random.randint(min, max))
+    return arr
+
+def num_list_as_int(numList) -> int:
+    return int(''.join(map(str, numList)))
+
+code1 = random_number_chain(3, 0, 9)
+code2 = random_number_chain(4, 1, 6)
+
+print(f"Code 1: {num_list_as_int(code1)} {code1}")
+print(f"Code 2: {num_list_as_int(code2)} {code2}")
